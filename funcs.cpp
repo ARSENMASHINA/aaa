26ca081bc81788b7288a3a664e3bda6769cb99cf
Arseniy Aphonin <ars.20013@yandex.ru>
2021-11-12 10:01:51 +0300

    add: myPow function

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "funcs.h"
+#include <cmath>
 
 double MySum(double a, double b)
 {
@@ -14,10 +15,7 @@ double MyMul(double a, double b)
     return a * b;
 }
 
-int MyPow(int num, int n) {
-    int res = num;
-    for (int i = 1; i < n; i++) {
-        res *= num;
-    }
-    return res;
+double MyPow(double a, double b)
+{
+    return exp(b * log(a));
 }
